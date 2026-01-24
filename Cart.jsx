8246c3a22ddb3ccf481413371b399e0ce73fb37b
Hamda-sule <157469503+Hamda-sule@users.noreply.github.com>
2026-01-24 13:53:51 +0300

    Add files via upload

---
+++ Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CartIterms from '../Components/CartIterms/CartIterms'
+
+export const Cart = () => {
+  return (
+    <div>
+     <CartIterms />
+    </div>
+  )
+}
+export default Cart 
